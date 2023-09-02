8e1c569eee853236330a57a5f9bde8225724c56c
sampofu022 <sampofu022@student.wethinkcode.co.za>
2023-09-02 22:48:54 +0200

    Added basic script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function getSurveyInfo(){
+    
+    let age = document.getElementById("age").value;
+    console.log("Age: " + age);
+    let gender = document.getElementById("gender").value;
+    console.log("gender: " + gender);
+    let field = document.getElementById("career").value;
+    console.log("field: " + field);
+    let ready = document.getElementById("hired?").value;
+    console.log("ready: " + ready);
+    let company = document.getElementById("company").value;
+    console.log("company: " + company);
+    let mentors = document.getElementById("mentors").value;
+    console.log("mentors: " + mentors);
+    let rate = document.getElementById("rate").value;
+    console.log("rate: " + rate);
+    let recommend = document.getElementById("net-promoter-score").value;
+    console.log("recommend: " + recommend);
+    alert("Thanks so much for provinding your insights and participating in my survey!")
+}
+
+function writeDataToJson(){
+    //TODO: WRITE THE FORM DATA INTO A JSON FILE.. I THINK
+}
+
+let submit = document.getElementById("submit").addEventListener("click", getSurveyInfo)
