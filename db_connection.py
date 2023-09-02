2dbcba791347d8e1b29ea921a1372622e84473f7
sampofu022 <sampofu022@student.wethinkcode.co.za>
2023-09-02 22:53:18 +0200

    minor tweaks

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,6 @@
 import mysql.connector as connector
+import json
+
 
 # connect to the mysql database: 
 db_connection = connector.connect(
@@ -10,3 +12,19 @@ cursor = db_connection.cursor()
 
 # create a new database:
 # cursor.execute("CREATE DATABASE survey")
+
+# TODO create the table schema:
+cursor.execute('''CREATE TABLE IF NOT EXIST responses(
+               id tiny-int primary-key auto_increment,
+               age tiny-int,
+               gender varchar(10),
+               isReady boolean,
+               feildOfInterest varchar(25),
+               companyOfInterest varchar(15),
+               mentorOpinion varchar(3),
+               scale tinyint
+               netPromoterScore tinyint
+               )'''
+            )
+
+#TODO: LOAD THE DATA FROM THE JSON FILE AND STORE IN THE DATABASE
