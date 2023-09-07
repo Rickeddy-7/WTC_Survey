2d26183b15b12ac0532c0228a248b4e104c18fc1
rickeddy <rickeddy3010@gmail.com>
2023-09-07 17:09:25 +0200

    changed to sqlite database

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # WTC_Survey
-
 A simple survey to gain insights on the genereal sentiments of students at WeThinkCode_ amidst
 placement season.
 
 ## GOAL
-The aim is conduct sentiment analysis on how ready they are, where they would like to work, 
+The aim is conduct analysis on how ready they are, where they would like to work, 
 do they find the course valuable, etc. I wanted to explore the process of data collection in a real life setting and apply my knowledge of data manipulation to try and draw indights as to how students are feeling and what they are thinking heading into the real world of software development.
 
 ## TOOLS
 Form built with basic HTML and CSS
-Database: MySQL (with Python connector)
-Visualisation: Tableau
+Database: SQLite
--- db_handler.py
+++ db_handler.py
@@ -1,76 +1,92 @@
 
-import mysql.connector as connector
-import socketserver
 import http.server as server
+import socketserver
+import sqlite3
 
 
 # DEFINE A HANDLER FOR THE HTTP REQUESTS:
 class DataHandler(server.SimpleHTTPRequestHandler):
 
     def do_POST(self):
+        '''connects to the database and adds new records to the table'''
+
         data = self.parseHTTP()
-        db_cursor = self.connectToDatabase()
-        
-        try:
-            query = f"""INSERT INTO responses (age, gender, isReady, fieldOfInterest, 
+        connection = self.connectToDatabase()
+        db_cursor = connection.cursor()
+        self.create_table(db_cursor)
+
+        query = f"""INSERT INTO responses (age, gender, isReady, feildOfInterest, 
                     companyOfInterest, mentorOpinion, scale, netPromoterScore) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?);"""
 
-            # values = (int(data['age']), data['gender'], data['isReady'], data['fieldOfInterest'],
-            #         data['companyOfInterest'], data['mentorOpinion'], 
-            #         int(data['scale']), int(data['netPromoterScore']))
+        values = (int(data['age']), data['gender'], data['ready'], data['career'],
+                data['company'], data['mentors'], int(data['curriculum']), int(data['nps']))
 
-            db_cursor.execute(query, data.values())
-
-            # save the changes to the database
-            db_cursor.connection.commit()
-
+        try:
+            db_cursor.execute(query, values)
+            # print('[REACHED]')
+            connection.commit()# save the changes to the database
+            for x in db_cursor: print(x)
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(b'Data inserted successfully!')
+
         except Exception as e:
             self.send_error(500, 'Internal Server Error', str(e))
+        
+        finally:
+            db_cursor.close()
+            connection.close()
 
 
     def connectToDatabase(self):
-        # connect to the mysql database: 
-        db_connection = connector.connect(host="localhost", user="root", database="survey")
-        # create a cursor object to query the database using the connection established above:
-        return db_connection.cursor()
+        '''returns a connection object to be used to curse the database'''
+        # create or connect to a database
+        return sqlite3.connect("survey.db")
 
-    def parseHTTP(self):
+
+    def parseHTTP(self) -> dict:
+        '''takes the request string, decodes it, and then seperates it into key-value pairs'''
+
+        # get the length of the request body:
         content_length = int(self.headers["Content-Length"])
-        print(f'[{content_length}]')
-        form_data = self.rfile.read(content_length).decode("utf-8")
-        print(f'[{form_data}]')
+        # then use it to determine how much data to read from the incoming request:
+        form_data: str = self.rfile.read(content_length).decode("utf-8")
         data = {}
         for kv_pair in form_data.split('&'):
             key, val = kv_pair.split('=')
             data[key] = val
 
         return data
+    
+
+    def create_table(self, cursor: sqlite3.Cursor):
+        
+        query = '''CREATE TABLE IF NOT EXISTS responses(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    age INTEGER,
+                    gender TEXT(10),
+                    isReady TEXT(3),
+                    feildOfInterest TEXT(25),
+                    companyOfInterest TEXT(15),
+                    mentorOpinion TEXT(3),
+                    scale INTEGER,
+                    netPromoterScore INTEGER
+                );'''
+        cursor.execute(query)
 
 
 
-# START THE SERVER TO LISTEN FOR REQUESTS FROM THE FORM:
-with socketserver.TCPServer(('localhost', 8000), DataHandler) as s:
-    print('Server running and listening on port 8000...\n')
-    s.serve_forever()
+def start_server():
+    '''start the server and accept requests from the form'''
+
+    PORT = 8000
+    with socketserver.TCPServer(('127.0.0.1', PORT), DataHandler) as s:
+        print(f'Server running and listening on port {PORT}...\n')
+        s.serve_forever()
 
 
 
-# HOW THE DATABASE WAS CREATED:
-# cursor.execute("CREATE DATABASE survey")
-
-# HOW THE TABLE SCHEMA WAS CREATED:
-# cursor.execute('''CREATE TABLE IF NOT EXISTS responses(
-#             id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-#             age TINYINT,
-#             gender VARCHAR(10),
-#             isReady VARCHAR(3),
-#             fieldOfInterest VARCHAR(25),
-#             companyOfInterest VARCHAR(15),
-#             mentorOpinion VARCHAR(3),
-#             scale TINYINT,
-#             netPromoterScore TINYINT);''')
+if __name__ == "__main__":
+    start_server()
